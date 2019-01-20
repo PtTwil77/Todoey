ea167b8743bfff4462fcc6cf0814e72ebfb5cf0c
Paul Terwilliger <paul.terwilliger83@gmail.com>
2019-01-19 22:46:02 -0500

    Add New Item functionality to To Do list.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Task A", "Task B", "Task C"]
+    var itemArray = ["Task A", "Task B", "Task C"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,5 +42,33 @@ class TodoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            print("Success")
+            if let newItem = textField.text {
+                self.itemArray.append(newItem)
+            }
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            print(alertTextField.text)
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
